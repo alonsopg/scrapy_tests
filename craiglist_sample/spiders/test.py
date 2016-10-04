3d7af62623679306bc221bbcfa564a3cf8391054
Alonso <alonsop@ciencias.unam.mx>
2016-10-04 15:59:11 -0500

    Add files via upload

--- craiglist_sample/spiders/test.py
+++ craiglist_sample/spiders/test.py
@@ -1,10 +1,11 @@
 from scrapy.selector import HtmlXPathSelector
+from scrapy import Selector
+import csv
 import pandas as pd
-from scrapy.spider import BaseSpider
-
+from scrapy.spiders import Spider
 # run with : scrapy crawl craig
 
-class MySpider(BaseSpider):
+class MySpider(Spider):
     name = "craig"
     #con craiglist
     allowed_domains = ["craigslist.org"]
@@ -12,9 +13,29 @@ class MySpider(BaseSpider):
 
 
     def parse(self, response):
-        hxs = HtmlXPathSelector(response)
-        titles = hxs.select("//span[@class='pl']")
+        hxs = Selector(response)
+        titles = hxs.xpath("//span[@class='pl']")
+        print ('\n*********Content********\n')
+        results = []
         for titles in titles:
-            title = titles.select("a/text()").extract()
-            link = titles.select("a/@href").extract()
-            print(title, link)
+            title = titles.xpath("a/text()").extract()
+            link = titles.xpath("a/@href").extract()
+            results.append([title, link])
+
+            #print('\naquí\n',len(results))
+
+        #with pandas, dump it into a .csv style
+
+        df = pd.DataFrame(results, columns=['A','B'])
+        for col in df.columns:
+            df[col] = df[col].apply(lambda i: ''.join(i))
+
+        df.to_csv('/Users/user/Desktop/new_dump.csv',index = False)
+
+
+        # #Dump it into a csv with csv module
+        # csv_file = open("/Users/user/Desktop/casas_dump4.csv",'w')
+        # wr = csv.writer(csv_file)
+        # for row in results:
+        #     wr.writerow(row)
+        #
